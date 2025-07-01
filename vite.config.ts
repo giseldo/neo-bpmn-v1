7e8579cdfc28454e0a8654c6c36830da7650ff76
Giseldo <giseldo@gmail.com>
2025-06-30 22:00:40 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/neo-bpmn-v1/', // Substitua pelo nome do seu repositório
+  base: '/neobpmn/', // Substitua pelo nome do seu repositório
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
